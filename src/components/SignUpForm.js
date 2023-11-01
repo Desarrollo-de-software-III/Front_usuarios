61ea1f146c5571a9bea6362ab318379347231cbd
Juan2040751 <viafara.juan@correounivalle.edu.co>
2023-11-01 14:31:19 -0500

    orquestacion

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -62,7 +62,7 @@ function SignUpModal({ onClose }) {
           console.log(userData);
 
           axios({
-            url: "http://users:8000/user/",
+            url: "http://localhost/api/users/create",
             method: "POST",
             headers: {
               "Content-Type": "application/json",
